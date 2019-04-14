8ec347c9ca87128416cd96559c3ee4e464130a38
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-04-15 00:49:22 +0530

    Add web and mobile react-router to components

---
+++ packages/components/src/router/index.js
@@ -0,0 +1,13 @@
+import {
+  NativeRouter,
+  Route as NativeRoute,
+  Link as NativeLink,
+  Redirect as NativeRedirect,
+  withRouter as NativeWithRouter
+} from "react-router-native";
+
+export let Router = NativeRouter;
+export let Route = NativeRoute;
+export let Link = NativeLink;
+export let Redirect = NativeRedirect;
+export let withRouter = NativeWithRouter;
