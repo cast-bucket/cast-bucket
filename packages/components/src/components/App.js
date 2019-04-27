76d8fe37f325c8910305ab62f56fe129e336dd52
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-04-28 02:47:44 +0530

    Fix App Component height and width

--- packages/components/src/components/App.js
+++ packages/components/src/components/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from "react";
 import styled from "@emotion/native";
 import { Provider } from "react-redux";
-import { Router, Route, Switch } from "../router";
+import { Router, Route, Switch } from "../libs/router";
 import configureStore from "../redux/store";
 
 import { Home } from "./screens";
 import Categories from "./layout/CategoryGrid";
+import { Dimensions } from "react-native";
 
 const initialState = {};
 const store = configureStore(initialState);
 
 // const action = type => store.dispatch({ type });
-
+const { width, height } = Dimensions.get("window");
 const Container = styled.View`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Texter = styled.Text`
-  font-family: Inter;
+  flex: 1;
+  width: ${width},
+  height: ${height},
 `;
 
 export class App extends Component {
@@ -29,8 +26,8 @@ export class App extends Component {
         <Router>
           <Container>
             <Switch>
-              <Route exact path="/choose-categories" component={Categories} />
-              <Route exact path="/" component={Home} />
+              <Route exact path="/" component={Categories} />
+              <Route exact path="/home" component={Home} />
             </Switch>
           </Container>
         </Router>
