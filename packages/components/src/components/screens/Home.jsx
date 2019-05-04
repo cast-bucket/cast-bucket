3c9a6e2b1a48791925d72b2fe3d458a8deb783dd
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-05-05 03:51:31 +0530

    WIP: Add Subscriptions section to Home

--- packages/components/src/components/screens/Home.jsx
+++ packages/components/src/components/screens/Home.jsx
@@ -1,10 +1,25 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { Button } from "../common/Button";
+import styled from "@emotion/native";
+import Podcasts from "../containers/Podcasts";
 
+const Container = styled.View`
+  margin: 30px;
+`;
+
+const Section = styled.View`
+  padding: 10px;
+`;
+
+const SectionTitle = styled.Text`
+  font-weight: 700;
+  font-size: 32px;
+  line-heigth: 30px;
+`;
 export const Home = props => (
-  <View>
-    <Text> Hello </Text>
-    <Button color="#d3d3d3" title="Click!" />
-  </View>
+  <Container>
+    <Section>
+      <SectionTitle>Subscriptions</SectionTitle>
+      <Podcasts type="subscriptions" />
+    </Section>
+  </Container>
 );
