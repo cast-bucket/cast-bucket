9bec527f7564c8baf7e77e31cd4b92e25e0b9163
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-05-13 02:45:41 +0530

    Added Heading, Title to Text Component

---
+++ packages/components/src/components/common/Text.jsx
@@ -0,0 +1,15 @@
+import styled from "@emotion/native";
+
+export const Text = styled.Text`
+  font-family: Inter;
+`;
+
+export const Title = styled(Text)`
+  font-size: 32px;
+  font-weight: 700;
+  line-height: 30px;
+`;
+
+export const Heading = styled(Title)`
+  font-size: 36px;
+`;
--- packages/components/src/components/common/index.js
+++ packages/components/src/components/common/index.js
@@ -1,2 +1,3 @@
 export { Button } from "./Button";
 export { CategoryItem } from "./CategoryItem";
+export { Heading, Text, Title } from "./Text";
