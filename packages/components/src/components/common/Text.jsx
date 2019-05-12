a90f7b7c40d7aa578a1e32303bc24ac9b2f6f27a
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-05-13 04:24:17 +0530

    Update Text styles for mobile

--- packages/components/src/components/common/Text.jsx
+++ packages/components/src/components/common/Text.jsx
@@ -1,15 +1,18 @@
 import styled from "@emotion/native";
+import { isMobile } from "../utils/platforms";
 
 export const Text = styled.Text`
   font-family: Inter;
+  color: #1d1d1d;
 `;
 
+const titleFontSize = isMobile ? "24px" : "32px;";
 export const Title = styled(Text)`
-  font-size: 32px;
+  font-size: ${titleFontSize};
   font-weight: 700;
-  line-height: 30px;
 `;
 
+const headingFontSize = isMobile ? "32px" : "36px;";
 export const Heading = styled(Title)`
-  font-size: 36px;
+  font-size: ${headingFontSize};
 `;
