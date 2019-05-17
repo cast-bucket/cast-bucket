43f1eb1e0a748a4d3a44fd2e8055018178ba46e0
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-05-18 04:19:45 +0530

    Add Paragraph to Typography

--- packages/components/src/components/common/Typography.jsx
+++ packages/components/src/components/common/Typography.jsx
@@ -1,27 +1,31 @@
-import styled from "@emotion/native";
 import React from "react";
-import { Headline, Text as RPText, Title as RPTitle } from "react-native-paper";
+import {
+  Headline,
+  Paragraph as RPParagraph,
+  Text as RPText,
+  Title as RPTitle
+} from "react-native-paper";
 import RF from "react-native-responsive-fontsize";
 
 const commonStyles = {
   fontFamily: "Inter"
 };
 
-export const Text = styled(RPText)`
-  font-family: Inter;
-`;
-
 const titleStyle = {
   fontSize: RF(3.5)
 };
 
 const headingStyle = {
-  fontSize: RF(4)
+  fontSize: RF(4.5)
 };
 
 export const Title = props => (
   <RPTitle {...props} style={[commonStyles, titleStyle, props.style]} />
 );
+
 export const Heading = props => (
   <Headline {...props} style={[commonStyles, headingStyle, props.style]} />
 );
+
+export const Paragraph = props => <RPParagraph {...props} style={[commonStyles, props.style]} />;
+export const Text = props => <RPText {...props} style={[commonStyles, props.style]} />;
