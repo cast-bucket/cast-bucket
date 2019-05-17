a23ac18af0f11a23e6f3841c72df9727526794b0
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-05-17 19:23:49 +0530

    Update homePage styles

--- packages/components/src/components/screens/Home.jsx
+++ packages/components/src/components/screens/Home.jsx
@@ -1,91 +1,87 @@
 import React from "react";
 import styled, { css } from "@emotion/native";
 import { Dimensions } from "react-native";
+import RF from "react-native-responsive-fontsize";
 import Podcasts from "../containers/Podcasts";
 import { Heading, Text, Title } from "../common";
 import { isMobile } from "../utils/platforms";
 
 const { width } = Dimensions.get("window");
 
-const Container = styled.ScrollView`
-  padding: ${isMobile ? "20px" : "30px"};
-`;
+const Container = styled.ScrollView``;
 
 const Section = styled.View`
-  padding: 10px;
-  margin-bottom: ${isMobile ? "0px" : "50px"};
-`;
-
-const RowButton = styled(Text)`
-  color: #1f1f1f;
-  font-size: 18px;
+  margin-top: ${isMobile ? "0px" : "50px"};
 `;
 
 const Row = styled.View`
   flex-direction: row;
   justify-content: space-between;
   width: ${width};
+  padding-left: 40px;
 `;
 
 const SectionTitle = styled(Title)`
-  width: ${width / 4}px;
+  font-weight: 700;
 `;
 
 const UserAvatar = styled.Image`
+  align-self: center;
   background: lightblue;
   border-radius: 100px;
-  width: 50px;
-  height: 50px;
-  align-self: center;
+  width: 40px;
+  height: 40px;
+  margin-right: 20px;
 `;
-
-const RowButtonText = "View All";
 
 // TODO: load more items, or link section to new page?
 const handleRowButtonPress = rowId => {
   console.log(rowId);
 };
 
+const RowButton = () => (
+  <Text style={{ fontSize: RF(2.5), marginRight: 15, alignSelf: "center" }}>View All</Text>
+);
+
 export const Home = props => (
   <Container>
-    <Row>
+    <Row
+      style={{
+        paddingTop: 50,
+        paddingBottom: 20
+      }}
+    >
       <Heading
         style={css`
-          padding: 40px 10px;
+          font-weight: 700;
         `}
       >
         Home
       </Heading>
       <UserAvatar source={{ uri: "https://i.pravatar.cc/120" }} />
     </Row>
-
     <Section>
       <Row>
         <SectionTitle>New Releases</SectionTitle>
-        <RowButton onPress={() => handleRowButtonPress("new-releases")}>{RowButtonText}</RowButton>
+        <RowButton onPress={() => handleRowButtonPress("new-releases")} />
       </Row>
       <Podcasts type="new-releases" />
     </Section>
     <Section>
       <Row>
         <SectionTitle>Subscriptions</SectionTitle>
-        <RowButton onPress={() => handleRowButtonPress("subscriptions")}>{RowButtonText}</RowButton>
       </Row>
       <Podcasts type="subscriptions" />
     </Section>
     <Section>
       <Row>
         <SectionTitle>Recommended</SectionTitle>
-        <RowButton onPress={() => handleRowButtonPress("recommended")}>{RowButtonText}</RowButton>
       </Row>
       <Podcasts type="recommended" />
     </Section>
     <Section>
       <Row>
         <SectionTitle>Recently Played</SectionTitle>
-        <RowButton onPress={() => handleRowButtonPress("recently-played")}>
-          {RowButtonText}
-        </RowButton>
       </Row>
       <Podcasts type="recently-played" />
     </Section>
