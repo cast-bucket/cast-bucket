f1e522eec46a653b417ca6e5ced608cce58da9fe
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-05-17 19:19:54 +0530

    Add rn-paper and rn-fontsize deps

--- packages/web/config-overrides.js
+++ packages/web/config-overrides.js
@@ -1,13 +1,15 @@
 const fs = require("fs");
 const path = require("path");
-const resolve = require("resolve");
 const webpack = require("webpack");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 
-const externalModules = [resolveApp("../../node_modules/react-native-super-grid")];
+const externalModules = [
+  resolveApp("../../node_modules/react-native-super-grid"),
+  resolveApp("../../node_modules/react-native-responsive-fontsize")
+];
 const appIncludes = [
   resolveApp("src"),
   resolveApp("../core/src"),
@@ -40,6 +42,30 @@ module.exports = function override(config, env) {
       reportFilename: "report.html"
     })
   );
+
+  config.module.rules.push({
+    test: /\.js$/,
+    exclude: /node_modules[/\\](?!react-native-paper|react-native-vector-icons|react-native-safe-area-view)/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        // Disable reading babel configuration
+        babelrc: false,
+        configFile: false,
+
+        // The configration for compilation
+        presets: [
+          ["@babel/preset-env", { useBuiltIns: "usage" }],
+          "@babel/preset-react",
+          "@babel/preset-flow"
+        ],
+        plugins: [
+          "@babel/plugin-proposal-class-properties",
+          "@babel/plugin-proposal-object-rest-spread"
+        ]
+      }
+    }
+  });
 
   return config;
 };
