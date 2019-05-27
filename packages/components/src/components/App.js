eca17e9d934a20f5b23890e6eb2fcde9b7e59d54
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-05-28 00:45:18 +0530

    Update App Component with Navigation Routes

--- packages/components/src/components/App.js
+++ packages/components/src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import styled from "@emotion/native";
-import { Dimensions } from "react-native";
+import { Dimensions, View } from "react-native";
 import { Provider } from "react-redux";
 import { Route, Switch, withRouter } from "../libs/router";
 import configureStore from "../redux/store";
+import BottomNavigation from "./navigation/BottomNavigation";
 import { Episodes, Home } from "./screens";
+import { Text } from "./common/Typography";
 
 const initialState = {};
 const store = configureStore(initialState);
@@ -16,15 +18,33 @@ const Container = styled.View`
   height: ${height},
 `;
 
+const Browse = () => <Text> Browse </Text>;
+const Library = () => <Text> Library </Text>;
+const Account = () => <Text> Account </Text>;
+
+const navigationRoutes = [
+  { key: "home", title: "Home", icon: "queue-music" },
+  { key: "browse", title: "Browse", icon: "queue-music" },
+  { key: "library", title: "Library", icon: "queue-music" },
+  { key: "account", title: "Account", icon: "queue-music" }
+];
+
 const AppView = props => {
   return (
     <Provider store={store}>
-      <Container>
-        <Switch>
-          <Route exact path="/episodes" component={Episodes} />
-          <Route exact path="/" component={Home} />
-        </Switch>
-      </Container>
+      <View style={{ flex: 1, flexDirection: "column" }}>
+        <Container>
+          <Switch>
+            <Route exact path={["/", "/home"]} component={Home} />
+            <Route exact path="/browse" component={Browse} />
+            <Route exact path="/library" component={Library} />
+            <Route exact path="/account" component={Account} />
+            <Route exact path="/episodes" component={Episodes} />
+            <Route exact path="/choose-categories" component={Episodes} />
+          </Switch>
+        </Container>
+        <BottomNavigation routes={navigationRoutes} />
+      </View>
     </Provider>
   );
 };
