15e5562ddf7c4d3c83f2e42a3b994ce20f001432
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-05-28 00:33:48 +0530

    Added BottomNavigation Component

---
+++ packages/components/src/components/navigation/BottomNavigation.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { BottomNavigation } from "react-native-paper";
+import { withRouter } from "../../libs/router";
+
+const Navigation = props => {
+  const { history, routes, location } = props;
+  const [navigationIndex, setNavigationIndex] = useState(0);
+  const [navigationRoutes] = useState(routes);
+
+  const handleIndexChange = index => setNavigationIndex(index);
+
+  const navState = {
+    index: navigationIndex,
+    routes: navigationRoutes
+  };
+
+  // avoid re-render when tabPress on currentLocation
+  const isCurrentLocation = route => {
+    return `/${route}` === location.pathname;
+  };
+
+  return (
+    <BottomNavigation
+      style={{ flex: 1, maxHeight: 54 }}
+      navigationState={navState}
+      onIndexChange={handleIndexChange}
+      renderScene={() => {}}
+      barStyle={{ backgroundColor: "#f3f3f3" }}
+      labeled={false}
+      onTabPress={({ route }) =>
+        !isCurrentLocation(route.key) && history.push({ pathname: `/${route.key}` })
+      }
+    />
+  );
+};
+
+export default withRouter(Navigation);
