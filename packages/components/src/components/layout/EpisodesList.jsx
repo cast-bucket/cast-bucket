d053a615a55aecb4e0cd76f13669eecf6bc70a54
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-06-10 01:29:51 +0530

    Replace RLView with simple FlatList in Episodes

--- packages/components/src/components/layout/EpisodesList.jsx
+++ packages/components/src/components/layout/EpisodesList.jsx
@@ -1,42 +1,23 @@
 import React, { Component } from "react";
-import { Dimensions, StyleSheet } from "react-native";
+import { Dimensions, FlatList, StyleSheet } from "react-native";
 import EpisodeItem from "../common/EpisodeItem";
-import { RecyclerView } from "../common/RecyclerView";
 
 const { width } = Dimensions.get("window");
 
 class EpisodesList extends Component {
   render() {
     const { data } = this.props;
-    const recyclerViewProps = {
-      showsHorizontalScrollIndicator: false,
-      useWindowScroll: true,
-      forceNonDeterministicRendering: true,
-      canChangeSize: true
-    };
-
-    const getRecyclerViewLayout = () => {
-      return (type, dim) => {
-        if (type === "EPISODE_ITEM") {
-          dim.width = width;
-          dim.height = 250;
-        }
-      };
-    };
-
-    const renderListItem = (type, data) => {
+    const renderListItem = data => {
       return <EpisodeItem {...data} />;
     };
-
     const containerStyle = styles.listContainer;
-    const recyclerViewData = data.items.map(item => ({ ...item, type: "EPISODE_ITEM" }));
+    const recyclerViewData = data.items.map(item => ({ ...item, key: item.link }));
     return (
-      <RecyclerView
-        data={recyclerViewData}
-        renderListItem={renderListItem}
-        layoutType={getRecyclerViewLayout()}
-        containerStyle={containerStyle}
-        recyclerViewProps={recyclerViewProps}
+      <FlatList
+        contentContainerStyle={containerStyle}
+        data={data.items}
+        renderItem={renderListItem}
+        keyExtractor={(item, index) => `${item.link}${index.toString()}`}
       />
     );
   }
@@ -44,10 +25,7 @@ class EpisodesList extends Component {
 
 const styles = StyleSheet.create({
   listContainer: {
-    minHeight: 1,
-    minWidth: 1,
-    flex: 1,
-    overflow: "hidden"
+    alignItems: "stretch"
   }
 });
 
