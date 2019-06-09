020027d2bf3eda0163346eb1eff25cef16ca5e6f
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-06-10 02:45:00 +0530

    Add comments to platforms for further work

--- packages/components/src/components/utils/platforms.js
+++ packages/components/src/components/utils/platforms.js
@@ -1,4 +1,6 @@
 import { Platform } from "react-native";
 
 export const isWeb = Platform.OS === "web";
+
+// TODO: Also use screenSizes to identify if mobile browser?
 export const isMobile = Platform.OS === "android" || Platform.OS === "ios";
