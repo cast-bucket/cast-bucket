3bd1729e4d63f6cc72cc5034eb0c06106b41ab3b
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-07-06 23:37:14 +0530

    Added isSmallScreen to platforms util

--- packages/components/src/components/utils/platforms.js
+++ packages/components/src/components/utils/platforms.js
@@ -1,6 +1,9 @@
-import { Platform } from "react-native";
+import { Platform, Dimensions } from "react-native";
+
+const { width } = Dimensions.get("screen");
 
 export const isWeb = Platform.OS === "web";
 
 // TODO: Also use screenSizes to identify if mobile browser?
 export const isMobile = Platform.OS === "android" || Platform.OS === "ios";
+export const isSmallScreen = isMobile || width <= 600;
