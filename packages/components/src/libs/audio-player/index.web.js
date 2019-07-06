f8fc71483afaabd1836ca3d88edbb8fee56dfe08
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-07-06 23:43:41 +0530

    Add web audio player using Howler.js

---
+++ packages/components/src/libs/audio-player/index.web.js
@@ -0,0 +1,294 @@
+import { Howl, Howler } from "howler";
+
+/**
+ * Player class containing the state of our playlist and where we are in it.
+ * Includes all methods for playing, skipping, updating the display, etc.
+ * @param {Array} playlist Array of objects with playlist song details ({title, file, howl}).
+ */
+
+class Player {
+  constructor() {
+    this.playlist = [];
+  }
+
+  add(playListItem) {
+    this.playlist.push(playListItem);
+  }
+  /**
+   * Play a song in the playlist.
+   * @param  {Number} index Index of the song in the playlist (leave empty to play the first or current).
+   */
+  play(index) {
+    const self = this;
+    let sound;
+
+    index = typeof index === "number" ? index : 0;
+    var data = self.playlist[index];
+
+    // If we already loaded this track, use the current one.
+    // Otherwise, setup and load a new Howl.
+    if (data.howl) {
+      sound = data.howl;
+    } else {
+      sound = data.howl = new Howl({
+        src: [data.link],
+        html5: true,
+        onplay: function() {
+          // Display the duration.
+          // duration.innerHTML = self.formatTime(Math.round(sound.duration()));
+
+          // Start upating the progress of the track.
+          requestAnimationFrame(self.step.bind(self));
+
+          // // Start the wave animation if we have already loaded
+          // wave.container.style.display = "block";
+          // bar.style.display = "none";
+          // pauseBtn.style.display = "block";
+        },
+        onload: function() {
+          // Start the wave animation.
+        },
+        onend: function() {
+          // Stop the wave animation.
+        },
+        onpause: function() {},
+        onstop: function() {},
+        onseek: function() {
+          // Start upating the progress of the track.
+          requestAnimationFrame(self.step.bind(self));
+        }
+      });
+    }
+
+    // Begin playing the sound.
+    sound.play();
+  }
+
+  /**
+   * Pause the currently playing track.
+   */
+  pause(index) {
+    var self = this;
+
+    // Get the Howl we want to manipulate.
+    const currentPlaying =
+      typeof index === "number" ? self.playlist[index] : self.playlist.find(item => item.howl);
+
+    if (currentPlaying && currentPlaying.howl) {
+      currentPlaying.howl.pause();
+    }
+  }
+
+  /**
+   * Skip to the next or previous track.
+   * @param  {String} direction 'next' or 'prev'.
+   */
+  skip(direction) {
+    var self = this;
+
+    // Get the next track based on the direction of the track.
+    var index = 0;
+    if (direction === "prev") {
+      index = self.index - 1;
+      if (index < 0) {
+        index = self.playlist.length - 1;
+      }
+    } else {
+      index = self.index + 1;
+      if (index >= self.playlist.length) {
+        index = 0;
+      }
+    }
+
+    self.skipTo(index);
+  }
+
+  /**
+   * Skip to a specific track based on its playlist index.
+   * @param  {Number} index Index in the playlist.
+   */
+  skipTo(index) {
+    var self = this;
+
+    const currentPlaying = self.playlist.filter(item => item.howl);
+
+    // Stop the current track.
+    if (currentPlaying && currentPlaying.howl) {
+      currentPlaying.howl.stop();
+    }
+
+    // Reset progress.
+    // progress.style.width = "0%";
+
+    // Play the new track.
+    self.play(index);
+  }
+
+  /**
+   * Set the volume and update the volume slider display.
+   * @param  {Number} val Volume between 0 and 1.
+   */
+  volume(val) {
+    var self = this;
+
+    // Update the global volume (affecting all Howls).
+    Howler.volume(val);
+  }
+
+  /**
+   * Seek to a new position in the currently playing track.
+   * @param  {Number} per Percentage through the song to skip.
+   */
+  seek(per) {
+    var self = this;
+
+    // Get the Howl we want to manipulate.
+    var sound = self.playlist.filter(item => item.howl);
+    // Convert the percent into a seek position.
+    if (sound.playing()) {
+      sound.seek(sound.duration() * per);
+    }
+  }
+
+  /**
+   * The step called within requestAnimationFrame to update the playback position.
+   */
+  step() {
+    var self = this;
+
+    // Get the Howl we want to manipulate.
+    const sound = self.playlist.find(item => item.howl).howl;
+
+    // Determine our current seek position.
+    var seek = sound ? sound.seek() : 0;
+    // // timer.innerHTML = self.formatTime(Math.round(seek));
+    // progress.style.width = ((seek / sound.duration()) * 100 || 0) + "%";
+
+    // If the sound is still playing, continue stepping.
+    if (sound && sound.playing()) {
+      requestAnimationFrame(self.step.bind(self));
+    }
+  }
+
+  /**
+   * Toggle the playlist display on/off.
+   */
+  togglePlaylist() {
+    var self = this;
+    var display = playlist.style.display === "block" ? "none" : "block";
+
+    setTimeout(
+      function() {
+        playlist.style.display = display;
+      },
+      display === "block" ? 0 : 500
+    );
+    playlist.className = display === "block" ? "fadein" : "fadeout";
+  }
+
+  /**
+   * Toggle the volume display on/off.
+   */
+  toggleVolume() {
+    var self = this;
+    var display = volume.style.display === "block" ? "none" : "block";
+
+    setTimeout(
+      function() {
+        volume.style.display = display;
+      },
+      display === "block" ? 0 : 500
+    );
+    volume.className = display === "block" ? "fadein" : "fadeout";
+  }
+
+  /**
+   * Format the time from seconds to M:SS.
+   * @param  {Number} secs Seconds to format.
+   * @return {String}      Formatted time.
+   */
+  formatTime(secs) {
+    var minutes = Math.floor(secs / 60) || 0;
+    var seconds = secs - minutes * 60 || 0;
+
+    return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+  }
+
+  clear() {
+    this.playlist = [];
+  }
+}
+
+const PlayerInstance = new Player();
+// Object.freeze(PlayerInstance);
+
+export default PlayerInstance;
+
+// var Player = function(playlist) {
+//   this.playlist = playlist;
+//   this.index = 0;
+
+//   // Display the title of the first track.
+//   track.innerHTML = "1. " + playlist[0].title;
+
+//   // Setup the playlist display.
+//   playlist.forEach(function(song) {
+//     var div = document.createElement("div");
+//     div.className = "list-song";
+//     div.innerHTML = song.title;
+//     div.onclick = function() {
+//       player.skipTo(playlist.indexOf(song));
+//     };
+//     list.appendChild(div);
+//   });
+// };
+
+// // Bind our player controls.
+// playBtn.addEventListener("click", function() {
+//   player.play();
+// });
+// pauseBtn.addEventListener("click", function() {
+//   player.pause();
+// });
+// prevBtn.addEventListener("click", function() {
+//   player.skip("prev");
+// });
+// nextBtn.addEventListener("click", function() {
+//   player.skip("next");
+// });
+// waveform.addEventListener("click", function(event) {
+//   player.seek(event.clientX / window.innerWidth);
+// });
+// playlistBtn.addEventListener("click", function() {
+//   player.togglePlaylist();
+// });
+// playlist.addEventListener("click", function() {
+//   player.togglePlaylist();
+// });
+// volumeBtn.addEventListener("click", function() {
+//   player.toggleVolume();
+// });
+// volume.addEventListener("click", function() {
+//   player.toggleVolume();
+// });
+
+// // Setup the event listeners to enable dragging of volume slider.
+// barEmpty.addEventListener("click", function(event) {
+//   var per = event.layerX / parseFloat(barEmpty.scrollWidth);
+//   player.volume(per);
+// });
+// sliderBtn.addEventListener("mousedown", function() {
+//   window.sliderDown = true;
+// });
+// sliderBtn.addEventListener("touchstart", function() {
+//   window.sliderDown = true;
+// });
+// volume.addEventListener("mouseup", function() {
+//   window.sliderDown = false;
+// });
+// volume.addEventListener("touchend", function() {
+//   window.sliderDown = false;
+// });
+
+// volume.addEventListener("mousemove", move);
+// volume.addEventListener("touchmove", move);
