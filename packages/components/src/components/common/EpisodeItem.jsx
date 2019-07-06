9620f0465b2d34def08eacb9341cdc856fc12424
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-07-06 23:45:02 +0530

    Add Player to EpisodesList; WIP: Resize when mount

--- packages/components/src/components/common/EpisodeItem.jsx
+++ packages/components/src/components/common/EpisodeItem.jsx
@@ -1,10 +1,11 @@
 import styled from "@emotion/native";
-import React from "react";
-import { View, Dimensions, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Dimensions, StyleSheet, TouchableOpacity } from "react-native";
 import { Text } from "./Typography";
+import { Feather as Icon } from "../../libs/vector-icons";
+import AudioPlayer from "../../libs/audio-player";
 
 const { width } = Dimensions.get("window");
-const PlayImage = require("../../assets/images/play-button.png");
 
 const EpisodeTitle = styled(Text)`
   color: #111111;
@@ -19,7 +20,7 @@ const EpisodeDate = styled(Text)`
   margin-vertical: 5px;
 `;
 
-const PlayButtonIcon = styled.Image`
+const PlayButtonIcon = styled(Icon)`
   align-self: flex-end;
   height: 30px;
   justify-content: center;
@@ -34,22 +35,38 @@ const Row = styled.View`
   justify-content: space-between;
 `;
 
-const PlayButtonContainer = styled.View`
-  flex: 1;
+const PlayButtonContainer = styled(TouchableOpacity)`
+  width: 30px;
+  height: 30px;
 `;
 
-const EpisodeItem = React.memo(({ item }) => {
+const EpisodeItem = React.memo(({ item, _index }) => {
   const { title, isoDate } = item;
+  const [isPlaying, setPlayingIndicator] = useState(false);
+
+  if (!item.enclosure || !item.enclosure.url) return;
 
   return (
     <View style={styles.episodeContainer}>
       <Row>
-        <View style={{ flexDirection: "column", maxWidth: width * 0.75 }}>
+        <View style={{ flexDirection: "column", maxWidth: 0.75 * width }}>
           <EpisodeTitle>{title}</EpisodeTitle>
           <EpisodeDate>{new Date(isoDate).toDateString()}</EpisodeDate>
         </View>
-        <PlayButtonContainer>
-          <PlayButtonIcon source={PlayImage} />
+        <PlayButtonContainer
+          onPress={() => {
+            setPlayingIndicator(!isPlaying);
+            isPlaying ? AudioPlayer.pause(_index) : AudioPlayer.play(_index);
+            // play(item.enclosure.url);
+          }}
+          underlayColor="#000"
+          activeOpacity={0.65}
+        >
+          <PlayButtonIcon
+            name={isPlaying ? "pause-circle" : "play-circle"}
+            size={30}
+            color="#3e70ff"
+          />
         </PlayButtonContainer>
       </Row>
     </View>
--- packages/components/src/components/layout/EpisodesList.jsx
+++ packages/components/src/components/layout/EpisodesList.jsx
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
 import { FlatList, StyleSheet } from "react-native";
 import EpisodeItem from "../common/EpisodeItem";
-
+import { isMobile } from "../utils/platforms";
 class EpisodesList extends Component {
+  resize = () => this.forceUpdate();
+
+  componentDidMount() {
+    if (!isMobile) {
+      window.addEventListener("resize", this.resize);
+    }
+  }
+
+  componentWillUnmount() {
+    if (!isMobile) {
+      window.removeEventListener("resize", this.resize);
+    }
+  }
+
   render() {
     const { data } = this.props;
-    const renderListItem = data => {
-      return <EpisodeItem {...data} />;
+    const renderListItem = ({ item, index }) => {
+      return <EpisodeItem item={item} _index={index} />;
     };
     const containerStyle = styles.listContainer;
+    // TODO: Lazy Load Episode Items onEndReached
     return (
       <FlatList
         contentContainerStyle={containerStyle}
