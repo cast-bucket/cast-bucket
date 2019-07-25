2385ac9d4136b6cff253a2a0415bb2691a2b4da7
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-07-25 19:11:40 +0530

    WIP: Move mock episodeItems out of render

--- packages/components/src/components/containers/Episodes.jsx
+++ packages/components/src/components/containers/Episodes.jsx
@@ -1,20 +1,18 @@
 import React, { Component } from "react";
 import * as mocks from "../../mocks";
 import EpisodesList from "../layout/EpisodesList";
-import AudioPlayer from "../../libs/audio-player";
+import Player from "../../libs/audio-player";
 
 const mockFeedId = "https://ryanripley.com/feed/";
+
+const AudioPlayer = new Player();
 
 class Episodes extends Component {
   constructor(props) {
     super(props);
     this.category = this.props.category;
-  }
-
-  render() {
-    // TODO: Replace mockEpisodes with sagas
-    // const { feed } = this.props;
     AudioPlayer.clear();
+
     if (mocks.episodeItems[mockFeedId]) {
       mocks.episodeItems[mockFeedId].items.forEach(item => {
         if (item && item.enclosure && item.enclosure.url) {
@@ -22,6 +20,11 @@ class Episodes extends Component {
         }
       });
     }
+  }
+
+  render() {
+    // TODO: Replace mockEpisodes with sagas
+    // const { feed } = this.props;
     return <EpisodesList data={mocks.episodeItems[mockFeedId]} />;
   }
 }
