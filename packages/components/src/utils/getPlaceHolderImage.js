ec216cd9df5abffd55df0287ec538391f5b76449
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-09-20 01:44:17 +0530

    Fix assets image path

--- packages/components/src/utils/getPlaceHolderImage.js
+++ packages/components/src/utils/getPlaceHolderImage.js
@@ -1,10 +1,11 @@
 const cards = [
-  require(`../../assets/images/cards/pattern-1.png`),
-  require(`../../assets/images/cards/pattern-2.png`),
-  require(`../../assets/images/cards/pattern-3.png`),
-  require(`../../assets/images/cards/pattern-4.png`)
+  require(`../assets/images/cards/pattern-1.png`),
+  require(`../assets/images/cards/pattern-2.png`),
+  require(`../assets/images/cards/pattern-3.png`),
+  require(`../assets/images/cards/pattern-4.png`)
 ];
 
+console.log('cards', cards)
 const numberOfImages = cards.length;
 
 export default stringId => {
