44cd27de96099472e63d5228b73f489e365924b6
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-09-20 01:47:30 +0530

    Update Player to play / pause correctly

--- packages/components/src/libs/audio-player/index.web.js
+++ packages/components/src/libs/audio-player/index.web.js
@@ -1,30 +1,50 @@
 import { Howl, Howler } from "howler";
 
 export default class Player {
-  constructor(url) {
-    if (url) {
-      this.audio = new Howl({ src: url });
+  constructor() {
+    this.meta = {};
+    this.player = {};
+  }
+
+  getPlayer(url) {
+    return this.player[url];
+  }
+
+
+  play(url) {
+    // set new audio source only if it's not already been played
+    if (!this.audio || this.audio._src !== url) {
+      this.setAudioSource(url);
     }
+    this.getPlayer(url)?.play();
+    this.lastPlayedEpisode = url;
   }
 
-  play(url = null) {
-    if (!this.audio && url) {
-      this.audio = new Howl({ src: url });
-    }
-    this.audio.play();
+  pause(url) {
+    this.lastPlayedEpisode = url;
+    return this.getPlayer(url)?.pause();
   }
 
-  pause() {
-    this.audio.pause();
-  }
-
-  seek(per) {
-    const duration = this.audio.duration();
-    this.audio.seek(duration * per);
-  }
+  // seek(per) {
+  //   const duration = this.audio.duration();
+  //   this.audio.seek(duration * per);
+  // }
 
   volume(val) {
     // Update the global volume (affecting all Howls).
     Howler.volume(val);
   }
+
+  setAudioSource(url) {
+    if (url && !this.player[url]) this.player[url] = new Howl({ src: url, html5: true });
+  }
+
+  setMetadata(data, url) {
+    this.meta[url] = data;
+  }
+
+  getMetadata(url) {
+    return this.meta[url]
+  }
+
 }
