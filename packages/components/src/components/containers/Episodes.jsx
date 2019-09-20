0050599e04a18efc90e5d6a3a19a69ff96b7086d
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-09-21 04:32:26 +0530

    Keep EpisodesContainer simple

--- packages/components/src/components/containers/Episodes.jsx
+++ packages/components/src/components/containers/Episodes.jsx
@@ -1,19 +1,6 @@
-import React, { Component } from "react";
-import * as mocks from "../../mocks";
+import React from "react";
 import EpisodesList from "../layout/EpisodesList";
 
-const mockFeedId = "https://ryanripley.com/feed/";
+const EpisodesContainer = ({ feed }) => <EpisodesList feed={feed} />;
 
-class Episodes extends Component {
-  constructor(props) {
-    super(props);
-    this.category = this.props.category;
-  }
-
-
-  render() {
-    return <EpisodesList data={mocks.episodeItems[mockFeedId]} />;
-  }
-}
-
-export default Episodes;
+export default EpisodesContainer;
