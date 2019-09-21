ea10f225a6e03334b98073f5672bff796feb323b
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-09-22 03:45:00 +0530

    Remove unnecessary EpisodesContainer

--- packages/components/src/components/containers/Episodes.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import EpisodesList from "../layout/EpisodesList";
-
-const EpisodesContainer = ({ feed }) => <EpisodesList feed={feed} />;
-
-export default EpisodesContainer;
