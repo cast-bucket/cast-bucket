d3887fab19d6781e1ac639fe69e8794f336b22ce
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-09-23 00:23:58 +0530

    Add types directory

---
+++ @types/react-native-vector-icons/index.d.ts
@@ -0,0 +1,11 @@
+declare module "react-native-vector-icons" {
+  export * from "react-native-vector-icons";
+}
+
+declare module "react-native-vector-icons/dist/MaterialIcons" {}
+
+declare module "react-native-vector-icons/dist/Feather" {}
+
+declare module "react-native-vector-icons/Fonts/MaterialIcons.ttf" {}
+
+declare module "react-native-vector-icons/Fonts/Feather.ttf" {}
