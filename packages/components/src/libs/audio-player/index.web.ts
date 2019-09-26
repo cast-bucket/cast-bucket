ed9f58bf787e69b86201b79a770c2787c4d66589
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-09-27 00:23:19 +0530

    Rename Howl Player with web extension

---
+++ packages/components/src/libs/audio-player/index.web.ts
@@ -0,0 +1,51 @@
+import { Howl, Howler } from "howler";
+
+interface AudioSource {
+  url: string;
+  howl: any | null;
+}
+
+export default class Player {
+  private audio: Array<AudioSource>;
+
+  constructor() {
+    this.audio = [];
+  }
+
+  getAudio(url: string) {
+    const source: AudioSource | undefined = this.audio.find((a: AudioSource) => a.url === url);
+    return source ? source.howl : null;
+  }
+
+  play(url: string) {
+    // set new audio source only if it's not already been played
+    if (!this.getAudio(url)) {
+      this.setAudioSource(url);
+    }
+    const player = this.getAudio(url);
+    return player.play();
+  }
+
+  pause(url) {
+    const player = this.getAudio(url);
+    player.pause();
+  }
+
+  // seek(per) {
+  //   const duration = this.audio.duration();
+  //   this.audio.seek(duration * per);
+  // }
+
+  volume(val: number) {
+    // Update the global volume (affecting all Howls).
+    Howler.volume(val);
+  }
+
+  setAudioSource(url: string) {
+    if (url)
+      this.audio.push({
+        url,
+        howl: new Howl({ src: url, html5: true })
+      });
+  }
+}
