caf382f3ce449e3a84570db0293e476ce3e8c882
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-09-28 04:11:12 +0530

    Use PodcastsSection in Home

--- packages/components/src/components/screens/Home.tsx
+++ packages/components/src/components/screens/Home.tsx
@@ -1,13 +1,9 @@
 import React from "react";
-import styled, { Styled } from "@emotion/native";
-import RF from "react-native-responsive-fontsize";
-import titleCase from "title-case";
+import styled from "@emotion/native";
 import { View } from "react-native";
-import { Title, Text } from "../common/Typography";
-import Podcasts from "../containers/Podcasts";
-import { isMobile } from "../../utils/platforms";
+import { Title } from "../common/Typography";
 import * as constants from "../../utils/constants";
-import { Link } from "../../libs/router";
+import PodcastsSection from "../layout/PodcastsSection";
 
 const Container = styled.ScrollView`
   flex: 1;
@@ -16,21 +12,6 @@ const Container = styled.ScrollView`
 const PageHeading = styled(Title)`
   font-weight: 700;
   margin-left: ${constants.spacing.containerMargin.dim};
-`;
-
-const Row = styled.View`
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: stretch;
-`;
-
-const Section = styled.View`
-  margin-top: 20px;
-`;
-
-const SectionTitle = styled(Title)`
-  font-weight: 700;
-  padding-left: ${constants.spacing.containerMargin.dim};
 `;
 
 const UserAvatar = styled.Image`
@@ -42,41 +23,12 @@ const UserAvatar = styled.Image`
   width: 50px;
 `;
 
-const rowButtonStyles: any = {
-  alignSelf: "center",
-  color: "#184277",
-  fontSize: RF(2.5),
-  marginRight: 15,
-  textDecoration: 'none' 
-};
-
-if (!isMobile) {
-  rowButtonStyles.cursor = "pointer";
-}
-
-// TODO: load more items, or link section to new page?
-const handleRowButtonPress = rowId => {
-  console.log("Row Row Row the boat at" + rowId);
-};
-
 const renderHomePageSections = () => {
-  const sections = ["new-releases", "subscriptions", "recommended", "recently-played"];
-  return sections.map(sectionId => {
-    return (
-      <Section key={sectionId}>
-        <Row>
-          <SectionTitle>{titleCase(sectionId)}</SectionTitle>
-          <Link style={{ ...rowButtonStyles }} onPress={() => handleRowButtonPress(sectionId)}>
-            <Text>View All</Text>
-          </Link>
-        </Row>
-        {
-          // @ts-ignore
-          <Podcasts type={sectionId} />
-        }
-      </Section>
-    );
-  });
+  const sections = ["new-releases", "subscriptions", "recently-played"];
+  // @ts-ignore
+  return sections.map((sectionId, index) => (
+    <PodcastsSection sectionType={sectionId} key={index} />
+  ));
 };
 
 export const Home = ({}) => (
@@ -90,7 +42,7 @@ export const Home = ({}) => (
         paddingTop: 40
       }}
     >
-      <PageHeading style={{ fontSize: RF(4) }}>Home</PageHeading>
+      <PageHeading style={{ fontSize: 32 }}>Home</PageHeading>
       <UserAvatar source={{ uri: "https://i.pravatar.cc/120" }} />
     </View>
     {renderHomePageSections()}
