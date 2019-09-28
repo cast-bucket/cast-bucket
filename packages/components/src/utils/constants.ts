44b704a24a5dcf8466c73b2b36033f02a6f6dc30
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-09-29 04:12:41 +0530

    Modify constants; Add appCategories to constants

--- packages/components/src/utils/constants.ts
+++ packages/components/src/utils/constants.ts
@@ -1,8 +1,35 @@
-const CONTAINER_MARGIN = 32;
+// data
+export const data = {
+  appCategories: [
+    "Artificial Intelligence",
+    "Agile",
+    "Android",
+    "Bsd",
+    "Career",
+    "Community & Inspiration",
+    "Data Management",
+    "Devops",
+    "Flutter",
+    "Functional Programming",
+    "Git",
+    "iOS",
+    "Kubernetes",
+    "Linux",
+    "Programming Languages & Frameworks",
+    "Security",
+    "Software Engineering",
+    "Software Testing",
+    "Web Development"
+  ]
+};
 
-export const spacing = {
-  containerMargin: {
-    val: CONTAINER_MARGIN,
-    dim: `${CONTAINER_MARGIN}px`
+// UI CONSTANTS
+const CONTAINER_MARGIN = 32;
+export const ui = {
+  containers: {
+    margin: {
+      value: CONTAINER_MARGIN,
+      px: `${CONTAINER_MARGIN}px`
+    }
   }
 };
