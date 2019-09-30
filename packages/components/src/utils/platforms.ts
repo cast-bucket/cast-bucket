c09edc1e00d2bcbc334881613cec4601d1ca678a
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-10-01 01:21:58 +0530

    Update isSmallScreen width breakpoint

--- packages/components/src/utils/platforms.ts
+++ packages/components/src/utils/platforms.ts
@@ -1,8 +1,9 @@
-import { Platform, Dimensions } from "react-native";
+import { Dimensions, Platform } from "react-native";
+import { breakpoints } from "./breakpoints";
 
 const { width } = Dimensions.get("screen");
 
 export const isWeb = Platform.OS === "web";
 
 export const isMobile = Platform.OS === "android" || Platform.OS === "ios";
-export const isSmallScreen = isMobile || width <= 600;
+export const isSmallScreen = isMobile || width <= breakpoints.MEDIUM_WIDTH;
