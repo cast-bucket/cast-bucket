31c695380184c008f3530613bdbd2b294c38d04b
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-10-05 23:32:25 +0530

    remove: unused sum cponent

--- packages/components/src/components/common/sum/sum.test.ts
+++
@@ -1,9 +0,0 @@
-
-describe("testing typescript with jest", () => {
-  it('adds 1 + 2 to equal 3 in TScript', () => {
-    const sum = require('./sum').default;
-    expect(sum(1, 2)).toBe(3);
-  });
-})
-
-
