1a41f6e83dd13a7f6d0c3608e018b6f5585d6cdc
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-12-17 01:53:45 +0530

    feat(theming): add ThemeProvider to root
    * add a default darkTheme
    * 🖤 Hello Darkness, my old friend!

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,13 +1,27 @@
 import { App } from "@cast-bucket/components/src/components/App";
 import { Router } from "@cast-bucket/components/src/libs/router";
+import { ThemeProvider } from "emotion-theming";
 import React from "react";
 import { AppRegistry } from "react-native-web";
 import "./index.css";
 
+const theme = {
+  colors: {
+    primary: "#2cb67d",
+    stroke: "#1b1e20",
+    secondary: "#72757e",
+    text: "#fffffe",
+    accent: "#9678f0",
+    background: "#101213"
+  }
+};
+
 const AppView = () => (
-  <Router>
-    <App />
-  </Router>
+  <ThemeProvider theme={theme}>
+    <Router>
+      <App />
+    </Router>
+  </ThemeProvider>
 );
 
 const render = (AppComponent: React.FC) => {
