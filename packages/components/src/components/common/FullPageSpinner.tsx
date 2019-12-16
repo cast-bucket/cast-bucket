c94bea197d2dc04c9c7187fbc28a33c9f8c180fa
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-12-17 01:54:43 +0530

    feat(theming): themify fullPageSpinner

--- packages/components/src/components/common/FullPageSpinner.tsx
+++ packages/components/src/components/common/FullPageSpinner.tsx
@@ -1,12 +1,20 @@
+import { useTheme } from "emotion-theming";
 import React from "react";
 import { ActivityIndicator, View } from "react-native";
 
 const FullPageSpinner = () => {
+  const theme: any = useTheme();
   return (
     <View
-      style={{ flex: 1, flexDirection: "column", justifyContent: "center", alignItems: "center" }}
+      style={{
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: theme.colors.background
+      }}
     >
-      <ActivityIndicator size="large" color="#0000ff" />
+      <ActivityIndicator size="large" color={theme.colors.accent} />
     </View>
   );
 };
