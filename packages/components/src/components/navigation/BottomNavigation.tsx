a9aab719016b9b70d8ecf292f4000445532238ac
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-12-17 01:58:08 +0530

    feat(theming): themify bottomNavigation
    * make bottomNavigation label bolder

--- packages/components/src/components/navigation/BottomNavigation.tsx
+++ packages/components/src/components/navigation/BottomNavigation.tsx
@@ -1,20 +1,21 @@
+import { useTheme } from "emotion-theming";
 import React, { useState } from "react";
-import { BottomNavigation } from "react-native-paper";
+import { BottomNavigation, Colors } from "react-native-paper";
 import { withRouter } from "../../libs/router";
 import { Feather as Icon } from "../../libs/vector-icons";
 import { Text } from "../common";
 
 const Navigation = props => {
   const { history, routes, location } = props;
+  const theme: any = useTheme();
   const [navigationIndex, setNavigationIndex] = useState(0);
   const [navigationRoutes] = useState(routes);
 
-  const handleIndexChange = index => setNavigationIndex(index);
+  const handleIndexChange = (index: number) => setNavigationIndex(index);
 
   const routesWithIcons = navigationRoutes.map(route => {
     return {
-      ...route,
-      icon: () => <Icon name={`${route.icon}`} size={24} color="#222" />
+      ...route
     };
   });
 
@@ -33,14 +34,20 @@ const Navigation = props => {
       navigationState={navState}
       onIndexChange={handleIndexChange}
       renderScene={() => null}
-      renderLabel={({ route }: any) => (
+      activeColor={theme.colors.accent}
+      inactiveColor={Colors.grey100}
+      renderIcon={({ route, color }: any) => (
+        <Icon name={`${route.icon}`} size={20} color={color} />
+      )}
+      renderLabel={({ route, color }: any) => (
         <Text
           style={{
             fontSize: 12,
+            fontWeight: "500",
             textAlign: "center",
             backgroundColor: "transparent",
             whiteSpace: "nowrap",
-            color: "#000000",
+            color,
             textTransform: "capitalize",
             marginTop: 3
           }}
@@ -48,7 +55,7 @@ const Navigation = props => {
           {route.key}
         </Text>
       )}
-      barStyle={{ backgroundColor: "#e3e3e3" }}
+      barStyle={{ backgroundColor: theme.colors.stroke }}
       onTabPress={({ route }: any) =>
         !isCurrentLocation(route.key) && history.replace({ pathname: `/${route.key}` })
       }
