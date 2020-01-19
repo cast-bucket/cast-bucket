61b5266a3e0517f20eb5c8d06c728f9bcb6383e0
Shriram Balaji <shrirambalaji1996@gmail.com>
2020-01-19 12:22:49 +0530

    chore(libs): export hooks from react-router

--- packages/components/src/libs/router/index.ts
+++ packages/components/src/libs/router/index.ts
@@ -1,4 +1,16 @@
-import { BackButton as NativeBackButton, Link as NativeLink, NativeRouter, Redirect as NativeRedirect, Route as NativeRoute, Switch as NativeSwitch, withRouter as NativeWithRouter } from "react-router-native";
+import {
+  BackButton as NativeBackButton,
+  Link as NativeLink,
+  NativeRouter,
+  Redirect as NativeRedirect,
+  Route as NativeRoute,
+  RouteComponentProps as NativeRouteComponentProps,
+  Switch as NativeSwitch,
+  useHistory as NativeUseHistory,
+  useLocation as NativeUseLocation,
+  useParams as NativeUseParams,
+  withRouter as NativeWithRouter
+} from "react-router-native";
 
 export const Router = NativeRouter;
 export const Route = NativeRoute;
@@ -7,3 +19,7 @@ export const Redirect = NativeRedirect;
 export const withRouter = NativeWithRouter;
 export const Switch = NativeSwitch;
 export const BackButton = NativeBackButton;
+export const useHistory = NativeUseHistory;
+export const useLocation = NativeUseLocation;
+export const useParams = NativeUseParams;
+export interface RouteComponentProps extends NativeRouteComponentProps {}
--- packages/components/src/libs/router/index.web.ts
+++ packages/components/src/libs/router/index.web.ts
@@ -1,4 +1,15 @@
-import { BrowserRouter, Link as WebLink, Redirect as WebRedirect, Route as WebRoute, Switch as WebSwitch, withRouter as WebWithRouter } from "react-router-dom";
+import {
+  BrowserRouter,
+  Link as WebLink,
+  Redirect as WebRedirect,
+  Route as WebRoute,
+  RouteComponentProps as WebRouteComponentProps,
+  Switch as WebSwitch,
+  useHistory as WebUseHistory,
+  useLocation as WebUseLocation,
+  useParams as WebUseParams,
+  withRouter as WebWithRouter
+} from "react-router-dom";
 
 export const Router = BrowserRouter;
 export const Route = WebRoute;
@@ -6,3 +17,7 @@ export const Link = WebLink;
 export const Redirect = WebRedirect;
 export const Switch = WebSwitch;
 export const withRouter = WebWithRouter;
+export const useHistory = WebUseHistory;
+export const useLocation = WebUseLocation;
+export const useParams = WebUseParams;
+export interface RouteComponentProps extends WebRouteComponentProps {}
