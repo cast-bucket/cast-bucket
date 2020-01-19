ea4e40de71cdee754f438c58a7a50358ddd2da0d
Shriram Balaji <shrirambalaji1996@gmail.com>
2020-01-19 11:58:50 +0530

    feat(theming): update light and darkTheme accents
    * add navigation color

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -5,26 +5,31 @@ import React from "react";
 import { AppRegistry } from "react-native-web";
 import "./index.css";
 
+// eslint-disable-next-line
 const lightTheme = {
   isDark: false,
   colors: {
     primary: "#2cb67d",
+    navigation: "#fffffe",
     stroke: "#eaeaea",
     secondary: "#2b2c34",
     text: "#2b2c34",
-    accent: "#6246ea",
+    accent: "#4e51e6",
     background: "#fffffe"
   }
 };
 
+// eslint-disable-next-line
 const darkTheme = {
   isDark: true,
   colors: {
     primary: "#2cb67d",
+    navigation: "#0b0d0e",
     stroke: "#1b1e20",
     secondary: "#72757e",
     text: "#fffffe",
-    accent: "#9678f0",
+    // accent: "#9678f0",
+    accent: "#959cff",
     background: "#101213"
   }
 };
