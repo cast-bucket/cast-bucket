ebcc8280bcd9e711fd48ce4634297c101d0c13ba
Shriram Balaji <shrirambalaji1996@gmail.com>
2020-01-22 03:22:16 +0530

    fix(libs): export useMatch from react-router

--- packages/components/src/libs/router/index.ts
+++ packages/components/src/libs/router/index.ts
@@ -1,10 +1,12 @@
 import {
   BackButton as NativeBackButton,
   Link as NativeLink,
+  matchPath as NativeMatchPath,
   NativeRouter,
   Redirect as NativeRedirect,
   Route as NativeRoute,
   RouteComponentProps as NativeRouteComponentProps,
+  RouteProps as NativeRouteProps,
   Switch as NativeSwitch,
   useHistory as NativeUseHistory,
   useLocation as NativeUseLocation,
@@ -16,10 +18,12 @@ export const Router = NativeRouter;
 export const Route = NativeRoute;
 export const Link = NativeLink;
 export const Redirect = NativeRedirect;
-export const withRouter = NativeWithRouter;
 export const Switch = NativeSwitch;
 export const BackButton = NativeBackButton;
+export const withRouter = NativeWithRouter;
+export const matchPath = NativeMatchPath;
 export const useHistory = NativeUseHistory;
 export const useLocation = NativeUseLocation;
 export const useParams = NativeUseParams;
 export interface RouteComponentProps extends NativeRouteComponentProps {}
+export interface RouteProps extends NativeRouteProps {}
--- packages/components/src/libs/router/index.web.ts
+++ packages/components/src/libs/router/index.web.ts
@@ -1,9 +1,11 @@
 import {
   BrowserRouter,
   Link as WebLink,
+  matchPath as WebMatchPath,
   Redirect as WebRedirect,
   Route as WebRoute,
   RouteComponentProps as WebRouteComponentProps,
+  RouteProps as WebRouteProps,
   Switch as WebSwitch,
   useHistory as WebUseHistory,
   useLocation as WebUseLocation,
@@ -17,7 +19,9 @@ export const Link = WebLink;
 export const Redirect = WebRedirect;
 export const Switch = WebSwitch;
 export const withRouter = WebWithRouter;
+export const matchPath = WebMatchPath;
 export const useHistory = WebUseHistory;
 export const useLocation = WebUseLocation;
 export const useParams = WebUseParams;
 export interface RouteComponentProps extends WebRouteComponentProps {}
+export interface RouteProps extends WebRouteProps {}
