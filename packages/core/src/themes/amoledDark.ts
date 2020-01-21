655936e7fcbb82e80fd3c76d4ec157b18e26092c
Shriram Balaji <shrirambalaji1996@gmail.com>
2020-01-22 03:23:24 +0530

    feat(themes): add stroke lighter, sidebar colours

--- packages/core/src/themes/amoledDark.ts
+++ packages/core/src/themes/amoledDark.ts
@@ -5,11 +5,15 @@ export const amoledDark: ITheme = {
   colors: {
     primary: "#2cb67d",
     navigation: "#000",
-    stroke: "#1b1e20",
+    // stroke: "#1b1e20",
+    sidebar: "#000",
+    stroke: "#131516",
+    strokeLighter: "#242729",
     secondary: "#72757e",
     text: "#fffffe",
     // accent: "#9678f0",
     accent: "#959cff",
-    background: "#000"
+    accentDarker: "#6670ff",
+    background: "#010101"
   }
 };
--- packages/core/src/themes/clearlyWhite.ts
+++ packages/core/src/themes/clearlyWhite.ts
@@ -6,9 +6,13 @@ export const clearlyWhite: ITheme = {
     primary: "#2cb67d",
     background: "#fcfcfc",
     stroke: "#eaeaea",
+    strokeLighter: "#e0e0e0",
     secondary: "#2b2c34",
+    sidebar: "#f3f3f3",
     text: "#2b2c34",
     accent: "#4e51e6",
+    accentLighter: "#a6a7f2",
+    accentDarker: "#4e51e6",
     navigation: "#fafafa"
   }
 };
