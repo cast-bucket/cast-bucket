4056c3a66db222fee292042c17462d5880db7507
Shriram Balaji <shrirambalaji1996@gmail.com>
2020-01-22 03:36:14 +0530

    fix(components):  update App container Styles
    * fix routes for /account, /history
    * move away ReduxProvider into context
    * show bottomNavigation only for smallScreens
    * show sidebarMenu only for larger screens

--- packages/components/src/components/App.tsx
+++ packages/components/src/components/App.tsx
@@ -1,80 +1,61 @@
+import { ITheme } from "@cast-bucket/core/src";
 import styled from "@emotion/native";
 import { useTheme } from "emotion-theming";
 import isEmpty from "is-empty";
 import React from "react";
-import { View } from "react-native";
-import { Provider } from "react-redux";
 import AppProviders from "../context";
 import { useAuthenticatedUser } from "../context/user";
-import { Route, Switch } from "../libs/router";
-import configureStore from "../redux/store";
+import { Route, Router, Switch } from "../libs/router";
+import { isSmallScreen } from "../utils/platforms";
 import FullPageSpinner from "./common/FullPageSpinner";
 import { Page } from "./common/Page";
 import Player from "./common/Player";
-import { PageHeading } from "./common/Typography";
 import BottomNavigation from "./navigation/BottomNavigation";
+import { SidebarNavigation } from "./navigation/SideBarNavigation";
 import { Browse, Downloads, Episodes, Home, Settings } from "./screens";
 import { Login } from "./screens/Login";
 import { Profile } from "./screens/Profile";
 
-const initialState = {};
-const store = configureStore(initialState);
-
 const AppContainer = styled.View`
-  flex-grow: 1;
-  flex-shrink: 1;
-  flex-basis: auto;
+  flex: 1;
+  align-items: stretch;
   flex-direction: column;
 `;
 
 const Container = styled.View`
   flex: 1;
-  align-self: auto;
+  justify-content: flex-start;
+  flex-direction: row;
+  align-items: stretch;
 `;
 
 const navigationRoutes = [
-  { key: "home", title: "Home", icon: "home" },
-  { key: "browse", title: "Browse", icon: "radio" },
-  { key: "profile", title: "Profile", icon: "user" }
+  { key: "home", title: "Home", icon: "home", Component: Home },
+  { key: "browse", title: "Browse", icon: "radio", Component: Browse },
+  { key: "profile", title: "Profile", icon: "user", Component: Profile }
 ];
 
 // TODO: Show navigation based on Platform Type
 const AuthenticatedApp = () => {
   // const user: any = useAuthenticatedUser();
-  const theme: any = useTheme();
+  const theme: ITheme = useTheme();
   return (
-    <Provider store={store}>
+    <Router>
       <AppContainer
         style={{
           backgroundColor: theme.colors.background
         }}
       >
         <Container>
+          {!isSmallScreen && <SidebarNavigation routes={navigationRoutes} />}
           <Switch>
             <Route exact path={["/", "/home"]} component={Home} />
             <Route exact path="/browse" component={Browse} />
-            <Route exact path="/downloads" component={Downloads} />
             <Route exact path="/profile" component={Profile} />
-            <Route
-              exact
-              path="/account"
-              render={() => (
-                <Page>
-                  <PageHeading>Account</PageHeading>
-                </Page>
-              )}
-            />
-            <Route
-              exact
-              path="/history"
-              render={() => (
-                <Page>
-                  <PageHeading>History</PageHeading>
-                </Page>
-              )}
-            />
+            <Route exact path="/downloads" component={Downloads} />
+            <Route exact path="/account" render={() => <Page title="Account" />} />
+            <Route exact path="/history" render={() => <Page title="History" />} />
             <Route exact path="/settings" render={() => <Settings />} />
-
             <Route
               path="/episodes/:podcastId"
               render={props => <Episodes key={props.match.params.podcastId} {...props} />}
@@ -83,9 +64,9 @@ const AuthenticatedApp = () => {
           </Switch>
         </Container>
         <Player />
-        <BottomNavigation routes={navigationRoutes} />
+        {isSmallScreen && <BottomNavigation routes={navigationRoutes} />}
       </AppContainer>
-    </Provider>
+    </Router>
   );
 };
 
@@ -94,7 +75,7 @@ const UnauthenticatedApp = () => <Login />;
 // TODO: Use HOC for Lazy Import
 // eslint-disable-next-line
 function LazyComponentHOC(Component) {
-  return props => (
+  return (props: any) => (
     <React.Suspense fallback={<FullPageSpinner />}>
       <Component {...props} />
     </React.Suspense>
